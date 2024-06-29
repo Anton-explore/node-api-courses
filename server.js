845bd12354f2403a17c85aaa02b021417a286c23
Anton Vladykin <famtripuae19@gmail.com>
2024-06-29 20:04:34 +0300

    feat: add CRUD operations for MongoDB, add error handler

--- server.js
+++ server.js
@@ -1,16 +1,24 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-
-// Route files
-
-const bootcamps = require('./routes/bootcamps')
+const colors = require('colors');
+const errorHandler = require('./middleware/error');
+const connectDB = require('./config/db');
 
 // Load environments
 dotenv.config({ path: './config/config.env' });
 
+// Connect to database
+connectDB();
+
+// Route files
+const bootcamps = require('./routes/bootcamps')
+
 // Initialize app
 const app = express();
+
+// Body parser
+app.use(express.json());
 
 // Dev logging middleware
 if (process.env.NODE_ENV === 'development') {
@@ -20,9 +28,19 @@ if (process.env.NODE_ENV === 'development') {
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
 
+app.use(errorHandler);
+
 const PORT = process.env.PORT || 6000;
 
-app.listen(
+const server = app.listen(
     PORT,
     console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`
+    .yellow.bold
 ));
+
+// Handle promise rejection
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error in promise: ${err.message}`.red);
+    // Close server end exit
+    server.close(() => process.exit(1));
+});
