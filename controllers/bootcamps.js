3005e592501076d6e00b8e510556e492e0534a16
Anton Vladykin <famtripuae19@gmail.com>
2024-10-28 21:24:12 +0200

    feat: add courses to bootcamp

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -16,7 +16,7 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
 // @route GET /api/v1/bootcamps/:id
 // @access Public
 exports.getBootcamp = asyncHandler(async (req, res, next) => {
-    const bootcamp = await Bootcamp.findById(req.params.id);
+    const bootcamp = await Bootcamp.findById(req.params.id).populate('courses');
 
     if (!bootcamp) {
         return next(new ErrorResponse(`Bootcamp with id: ${req.params.id} is not found`, 404));
